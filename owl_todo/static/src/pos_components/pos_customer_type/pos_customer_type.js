94fe16c438d717a7d9a79d8b647e5ac34e9a2a67
nimi-odoo <nimi@odoo.com>
2023-06-13 20:14:10 -0400

    [REM] unneeded code and components

--- owl_todo/static/src/pos_components/pos_customer_type/pos_customer_type.js
+++ owl_todo/static/src/pos_components/pos_customer_type/pos_customer_type.js
@@ -23,43 +23,6 @@ odoo.define("owlguy.PartnerDetailsEdit", function(require) {
             super.setup(...arguments)
         }
     }
-    // PartnerDetailsEdit.template = "owl.ResPartnerFormView2"
     Registries.Component.extend(PartnerDetailsEdit, PartnerDetailsEditInherit);
     return PartnerDetailsEdit;
 });
-
-
-
-/** @odoo-module **/
-
-// import { patch } from '@web/core/utils/patch';
-// import { PartnerDetailsEdit } from "@point_of_sale/js/Screens/PartnerListScreen/PartnerDetailsEdit";
-// import { PartnerDetailsEdit } from "@point_of_sale/js/";
-// import { PartnerDetailsEdit } from "@partner_autocomplete/js/partner_autocomplete_core"
-
-// import { registry } from "@web/core/registry";
-// import { kanbanView } from "@web/views/kanban/kanban_view";
-// import { useService } from "@web/core/utils/hooks";
-
-// const { onWillStart } = owl;
-// class maybeThis extends PartnerDetailsEdit {
-//     setup() {
-//         console.log("PartnerDetailsEdit loaded")
-//         super.setup();
-//     }
-// }
-
-// console.log("patching...")
-// patch(PartnerDetailsEdit.prototype, "matthew rwasenge", {
-//     setup() {
-//         console.log("hewo")
-//         super.setup();
-//         // this._super();
-//         onWillStart(console.log("hello there world"))
-//     },
-// });
-
-
-
-
-
