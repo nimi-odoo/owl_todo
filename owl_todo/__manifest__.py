f3149e2614b05ab54484615d79e6c22b6fbc97f2
nimi-odoo <nimi@odoo.com>
2023-06-20 12:34:58 -0400

    [ADD] New fields for res.partner for web form

--- owl_todo/__manifest__.py
+++ owl_todo/__manifest__.py
@@ -13,7 +13,7 @@
     'application': True,
     'category': 'OWL',
     'version': '0.1',
-    'depends': ['base', 'web', 'point_of_sale'],
+    'depends': ['base', 'web', 'point_of_sale', 'sale'],
     'data': [
         'security/ir.model.access.csv',
         'views/todo_list.xml',
@@ -24,6 +24,11 @@
             'owl_todo/static/src/components/*/*.js',
             'owl_todo/static/src/components/*/*.xml',
             'owl_todo/static/src/components/*/*.scss'
+        ],
+        'web.assets_frontend': [
+            'owl_todo/static/src/website_components/*/*.js',
+            'owl_todo/static/src/website_components/*/*.xml',
+            'owl_todo/static/src/website_components/*/*.scss'
         ],
         'point_of_sale.assets': [
             'owl_todo/static/src/pos_components/*/*.js',
--- owl_todo/models/res_partner.py
+++ owl_todo/models/res_partner.py
@@ -3,6 +3,13 @@ from odoo import api, fields, models
 class ResPartner(models.Model):
     _inherit="res.partner"
 
+    # point-of-sale
     customer_type = fields.Selection(string="Customer Type", selection=[("consumer", "Consumer"), ("patient", "Patient"), ("caregiver", "Caregiver"), ("external_patient", "External Patient")])
+
+    # website
+    # date_of_birth = fields.Date(string="Date of Birth")
+    # gender = fields.Selection(string="Gender", selection=[("hombre", "Hombre"), ("mujer", "Mujer")])
+    # id_number = fields.Char()
+    # barrio = fields.Char()
 
 
