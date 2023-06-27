c637d002d87a376329db779580ffe70a94990cf7
nimi-odoo <nimi@odoo.com>
2023-06-27 09:38:30 -0400

    [FIX] xpathing issue and street2 required

--- owl_todo/controllers/main.py
+++ owl_todo/controllers/main.py
@@ -3,10 +3,10 @@ from odoo.addons.website_sale.controllers.main import WebsiteSale
 class WebsiteSaleInherit(WebsiteSale):
     def _get_mandatory_fields_billing(self, country_id=False):
         req = super()._get_mandatory_fields_billing(country_id)
-        req += ['mobile', 'date_of_birth', 'id_number', 'barrio']
+        req += ['mobile', 'date_of_birth', 'id_number', 'barrio', 'street2']
         return req
 
     def _get_mandatory_fields_shipping(self, country_id=False):
         req = super()._get_mandatory_fields_shipping(country_id)
-        req += ['mobile', 'date_of_birth', 'id_number', 'barrio']
+        req += ['mobile', 'date_of_birth', 'id_number', 'barrio', 'street2']
         return req
